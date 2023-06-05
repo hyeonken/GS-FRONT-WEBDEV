ea09057bfc39205ab12cc4f5a412f71c9436b4e4
tomaz versolato carballo <RM551417@fiap.com.br>
2023-06-05 12:09:30 -0300

    O final verdadeiro.

---
+++ js/faleconosco.js
@@ -0,0 +1,7 @@
+const btnToggle = document.querySelector("#check")
+const corbody = document.querySelector("body")
+
+
+btnToggle.addEventListener("click", ()=> {
+    corbody.classList.toggle("dark-mode")
+})
